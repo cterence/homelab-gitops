4473cb78d28fd783c502bd717f15205079512fbc
Térence Chateigné <terence.chateigne@posteo.net>
2026-04-05 19:36:41 +0200

    feat(scrobble-dedup): use openbao store

--- terraform/secrets/main.tf
+++ terraform/secrets/main.tf
@@ -27,6 +27,70 @@ resource "vault_mount" "kvv2" {
   description = "KV Version 2 secret engine mount"
 }
 
+resource "vault_auth_backend" "kubernetes" {
+  type = "kubernetes"
+}
+
+resource "kubernetes_cluster_role_binding_v1" "openbao_auth_delegator" {
+  metadata {
+    name = "openbao-auth-delegator-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "system:auth-delegator"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "openbao"
+    namespace = "openbao"
+  }
+}
+
+resource "kubernetes_secret_v1" "openbao_auth_token" {
+  metadata {
+    name      = "openbao-auth-reviewer-token"
+    namespace = "openbao"
+    annotations = {
+      "kubernetes.io/service-account.name" = "openbao"
+    }
+  }
+  type = "kubernetes.io/service-account-token"
+}
+
+resource "vault_kubernetes_auth_backend_config" "example" {
+  backend                       = vault_auth_backend.kubernetes.path
+  kubernetes_host               = "https://kubernetes.default.svc"
+  kubernetes_ca_cert            = kubernetes_secret_v1.openbao_auth_token.data["ca.crt"]
+  token_reviewer_jwt_wo         = kubernetes_secret_v1.openbao_auth_token.data["token"]
+  token_reviewer_jwt_wo_version = 1
+  issuer                        = "api"
+  disable_iss_validation        = true
+}
+
+resource "vault_policy" "external_secrets_policy" {
+  name   = "external-secrets-policy"
+  policy = <<-EOT
+    path "${vault_mount.kvv2.path}/data/*" {
+      capabilities = ["read"]
+    }
+
+    path "${vault_mount.kvv2.path}/metadata/*" {
+      capabilities = ["list", "read"]
+    }
+    EOT
+}
+
+resource "vault_kubernetes_auth_backend_role" "external_secrets" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "external-secrets"
+  bound_service_account_names      = ["external-secrets"]
+  bound_service_account_namespaces = ["external-secrets"]
+  token_ttl                        = 3600
+  token_policies                   = ["default", vault_policy.external_secrets_policy.name]
+  audience                         = "openbao"
+}
+
 resource "vault_kv_secret_v2" "secrets" {
   for_each = nonsensitive(local.vault_map)
 
--- terraform/secrets/providers.tf
+++ terraform/secrets/providers.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/vault"
       version = "5.8.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.0.1"
+    }
   }
 }
 
@@ -17,4 +21,9 @@ provider "sops" {}
 
 provider "vault" {
   address = "http://localhost:8200"
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "k8s-tailscale-operator.snow-delta.ts.net"
 }
