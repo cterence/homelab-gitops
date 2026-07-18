6a01c61ac897ea576ce5eb37db6073fbeadf0203
Térence Chateigné <terence.chateigne@posteo.net>
2026-07-18 12:11:26 +0200

    scripts: remove outdated bootstrap/uninstall

--- scripts/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e -u -o pipefail
-
-# Uninstall the Kubernetes cluster, remove the config files, reset iptables and restart docker, kubelet and containerd
-
-ssh -tt terence@homelab "sudo kubeadm reset -f; sudo rm -rf /etc/cni /etc/kubernetes /var/lib/dockershim /var/lib/etcd /var/lib/kubelet /var/run/kubernetes ~/.kube/*; sudo iptables -F; sudo iptables -t nat -F; sudo iptables -t mangle -F; sudo iptables -X; sudo systemctl restart docker kubelet containerd"
